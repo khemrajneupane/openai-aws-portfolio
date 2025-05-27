af1457d074a01583418c2d3a3bc6ba6dd42e15c0
Khem Raj Neupane <shekharneupane@gmail.com>
2025-05-27 14:17:49 +0300

    info text adds to chat new/external window popup

--- app/socketio-chat-popup/page.tsx
+++ app/socketio-chat-popup/page.tsx
@@ -4,6 +4,10 @@ import React from "react";
 const LoginPage = () => {
   return (
     <div className="w-1/2 mx-auto">
+      <h1>
+        This is a new window for demo purpose, you can track chats in the main
+        window
+      </h1>
       <SocketChat />
     </div>
   );
